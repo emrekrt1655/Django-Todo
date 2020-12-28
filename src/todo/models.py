6c0df33600e1ff52ae5468deadf07ed50608a6c0
Emre <emrekurtt1655@gmail.com>
2020-12-28 21:33:01 +0100

    add create_todo

--- src/todo/models.py
+++ src/todo/models.py
@@ -5,6 +5,9 @@ class Todo(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
     
+    class Meta:
+        ordering = ('-created_date',) 
+    
     def __str__(self):
         return self.title
     
--- src/todo/views.py
+++ src/todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoAddForm
 def home(request):
     return render(request, "todo/home.html")
 
@@ -9,3 +10,16 @@ def todo_list(request):
         'todos': todos
     }
     return render(request, "todo/todo_list.html", context)
+
+def todo_create(request):
+    form = TodoAddForm()
+    if request.method == "POST":
+        form = TodoAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("todo-list")
+    context = {
+        'form': form
+    }
+    return render(request, "todo/todo_create.html", context)
+    
